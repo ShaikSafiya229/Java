58a67b6144deca90812c97f2416a36384716c44a
ShaikSafiya229 <safiyashaik2006@gmail.com>
2026-06-23 09:37:11 +0530

    java

--- File_Handling.java
+++ File_Handling.java
@@ -1,13 +1,21 @@
-import java.io.File;
-public class File_Handling {
-    public static void main(String[] args)
-     {
-        try{
-        File Data =new File("Sample.java");
-        Data.delete();
-        }
-        catch(Exception e){
+import java.io.BufferedReader;
+import java.io.FileReader;
+public class File_Handling 
+{
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            BufferedReader slide = new BufferedReader(new FileReader("Daemon.java"));
+            String Line;
+            while((Line=slide.readLine())!=null)
+                {
+                    System.out.println(Line);
+                }
+                slide.close();               
+        } catch (Exception e) {
             System.out.println(e);
         }
-    }
+        
+    }    
 }
